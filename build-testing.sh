8bb1e0fd4fbf95156ff25442f93f5ac459306562
Marco <marco@mjanssen.nl>
2025-01-31 22:34:19 +0100

    20250130

--- build-testing.sh
+++ build-testing.sh
@@ -5,7 +5,7 @@
 # date: 2021-11-28 14:24:26
 # update: 2021-11-28 14:24:32
 
-VERSION="24.6-testing-20241223"
+VERSION="24.6-testing-20250130"
 
 docker image rm marc0janssen/nzbgetvpn:testing
 docker image rm marc0janssen/nzbgetvpn:${VERSION}
